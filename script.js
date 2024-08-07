8db5765d1fb9aaf75814ee2db43dbff1bb5b41bd
Nkzn07 <163937348+Nkzn07@users.noreply.github.com>
2024-08-07 00:51:57 +0000

    aa

---
+++ script.js
@@ -0,0 +1,13 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+const pergunta = [
+    enunciado:"você é um ser humano?",
+    alternativas;[
+    "sim",
+    "não"
+]
+     ];`
+     
